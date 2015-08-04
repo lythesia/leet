98efe752f82279a1c0adc8e9bcda56e6511a8847
lythesia <iranaikimi@gmail.com>
2015-08-04 18:22:59 +0800

    delete-node-in-a-linked-list

---
+++ delete-node-in-a-linked-list/delete-node-in-a-linked-list.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define inject(x) { cerr << "Function: " << __FUNCTION__ << ", Line: " << __LINE__ << ", " << #x << ": " << (x) << endl; }
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+
+class Solution {
+public:
+  void deleteNode(ListNode *node) { *node = *node->next; }
+};
