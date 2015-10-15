2813b94da8e4f8f9ab96556a32d10942450cdf5f
lythesia <iranaikimi@gmail.com>
2015-10-16 00:12:49 +0800

    nim-game

---
+++ nim-game/nim-game.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define inject(x) { cerr << "Function: " << __FUNCTION__ << ", Line: " << __LINE__ << ", " << #x << ": " << (x) << endl; }
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+
+class Solution {
+  public:
+    bool canWinNim(int n) {
+      return n % 4 != 0;
+    }
+};
