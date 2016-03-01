c9f5e07caecda0d52aa3729aa72f88a4e810c086
lythesia <iranaikimi@gmail.com>
2016-03-01 21:03:16 +0800

    power-of-three

---
+++ power-of-three/power-of-three.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define inject(x) { cerr << "Function: " << __FUNCTION__ << ", Line: " << __LINE__ << ", " << #x << ": " << (x) << endl; }
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<string> vs;
+typedef vector<vs> vvs;
+
+class Solution {
+public:
+  bool isPowerOfThree(int n) {
+    return n > 0 && !(1162261467 % n); // 3**19 <= INT_MAX
+  }
+};
